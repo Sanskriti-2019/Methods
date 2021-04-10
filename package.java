7dbc665f5631aa244d326e940f05e2f768a711e7
Sanskriti Raj <75525184+Sanskriti-2019@users.noreply.github.com>
2021-04-10 23:49:39 +0530

    created package.java
    This file converts the number of minutes into number of years and days.

---
+++ package.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+
+public class Assignment{
+    public static void main(string[] args){
+      Scanner sc = new Scanner (System.in);
+      System.out.println("Enter number of minutes =");
+      double min = sc.nextDouble();
+
+
+      long years =(long)(min/(60*24*365));
+      long days =(long)(min/60/24)%365;
+
+      System.out.println(min +"is approx" + years + "years &" + days "days")
+
+}
+      
+    
+
